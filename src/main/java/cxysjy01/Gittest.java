40b5ba94d274238d339e25fc659fb56e0b1f4ec4
sjy <sjy@buguai.com>
2025-12-07 13:51:39 +0800

    hot-fix commit1

--- src/main/java/cxysjy01/Gittest.java
+++ src/main/java/cxysjy01/Gittest.java
@@ -12,5 +12,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("better");
         System.out.println("perfect");
+        System.out.println("888");
     }
 }
