a6f9b2fc1d1bd20f2162642b0692aee4eb0b168e
sjy <sjy@buguai.com>
2025-12-07 11:47:32 +0800

    first commit

---
+++ src/main/java/cxysjy01/Gittest.java
@@ -0,0 +1,15 @@
+package cxysjy01;
+
+public class Gittest {
+
+    //psvm + 回车 可以快速生成
+    //  public static void main(String[] args) {}
+
+    //添加到暂存区也就是使用git
+    //1.可以单独添加这个文件
+    //2.可以把整个项目添加到git
+
+    public static void main(String[] args) {
+        System.out.println("better");
+    }
+}
